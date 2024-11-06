d3e12a3c592e8bb40584f4e77cf9e50757be98a7
olgagatillo <klover091@gmail.com>
2024-11-06 15:55:23 +0300

    Update change_pas.js

--- change_pas.js
+++ change_pas.js
@@ -1,17 +1,17 @@
 function change_data(){
 
 
-    if (document.getElementsByTagName('p')[4].innerHTML==='GATILLO'){
+    if (document.getElementsByTagName('p')[4].innerHTML==='GATILLO')
+    {
         document.getElementsByTagName('p')[4].innerHTML='ГАТИЛЛО';
         document.getElementsByTagName('p')[5].innerHTML='ОЛЬГА';
         document.getElementsByTagName('p')[6].innerHTML='ВЛАДИМИРОВНА';
-        return
     }
-    if (document.getElementsByTagName('p')[4].innerHTML==='ГАТИЛЛО'){
+    if (document.getElementsByTagName('p')[4].innerHTML==='ГАТИЛЛО')
+    {
         document.getElementsByTagName('p')[4].innerHTML='GATILLO';
         document.getElementsByTagName('p')[5].innerHTML='OLGA';
         document.getElementsByTagName('p')[6].innerHTML='VLADIMIROVNA';
-        return
 
     }
 }
